e933bf2bf1533b8430d1ffa58f2f1e4d5fe220a4
Hiteksha Godhani <hkakadiya23@gmail.com>
2025-04-08 10:56:52 -0700

    Initial Commit

---
+++ CIExampleApp/CIExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIExampleAppApp.swift
+//  CIExampleApp
+//
+//  Created by Hiteksha Godhani on 2025-04-08.
+//
+
+import SwiftUI
+
+@main
+struct CIExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIExampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CIExampleApp
+//
+//  Created by Hiteksha Godhani on 2025-04-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
